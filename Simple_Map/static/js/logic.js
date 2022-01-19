47ae093fdf56e011f04ad516ac2d834c4e696be6
manBow1119 <brannonb3@gmail.com>
2022-01-18 18:06:15 -0600

    adding files and gitignore

---
+++ Simple_Map/static/js/logic.js
@@ -0,0 +1,16 @@
+
+
+console.log("working");
+//create the map object with a center and zoom level
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//create tile layer that will be background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
